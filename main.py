5475f37866b956ff361dc92379345ab9dd44ad7c
ruslan <ruslan@mail.ru>
2024-12-03 18:53:24 +0300

    Создать приложение

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class Suprematism(QWidget):
         if (event.button() == Qt.LeftButton):
             self.status = 1
         elif (event.button() == Qt.RightButton):
-            self.status = 2
+            self.status = 3
         self.drawf()
 
     def mouseMoveEvent(self, event):
